b9529517d97e6e72fc0e093a12f7402c00eabf56
TatiMartinez4 <tatianainesmartinez@gmail.com>
2025-04-26 22:23:41 -0300

    Subida inicial de 00-CHelloWorld

---
+++ README.md
@@ -0,0 +1,8 @@
+## Compilador seleccionado:
+- GCC
+
+## Version del compilador:
+- 14.2.0
+
+## Version de C que compila:
+- C11, C17 y C23
---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Nombre: hola.c
+ * Descripción: programa que imprime un hola mundo por pantalla
+ * Autora: Tatiana Inés Martínez
+ * Fecha de última modificación: 26/04/2025
+ */
+
+int main()
+{
+    printf("hola mundo\n");
+    return 0;   
+}
